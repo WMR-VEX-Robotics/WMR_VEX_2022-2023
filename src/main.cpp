890c330244ea5adb900a9ebb5d1e40c0ac3d1e06
ronan3706 <24_rfowler@s.wmrhsd.org>
2022-10-04 16:22:39 -0400

    hi buddy

--- src/main.cpp
+++ src/main.cpp
@@ -6,101 +6,49 @@
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-
 #include "vex.h"
-
 using namespace vex;
-
-// A global instance of competition
 competition Competition;
-
-/// A global instance of brain used for printing to the V5 Brain screen
 brain Brain;
-
-// VEXcode device constructors
-controller Controller1 = controller(primary);
-motor LeftFront = motor(PORT1, ratio36_1, false);
-motor LeftRear = motor(PORT2, ratio18_1, false);
-motor RightFront = motor(PORT3, ratio18_1, false);
-motor RightRear = motor(PORT4, ratio18_1, false);
-pneumatics P1 = pneumatics(Brain.ThreeWirePort.F);
-
-// define variable for remote controller enable/disable
+controller controlla = controller(primary);
+motor motor1 = motor(PORT1, ratio36_1, false);
+/*
+motor [name] = motor(PORT2, ratio18_1, false);
+motor [name] = motor(PORT3, ratio[?]_1, false);
+motor [name] = motor(PORT4, ratio36_1, false);
+pneumatics name = pneumatics(Brain.ThreeWirePort.[lettter]);
+*/
 bool RemoteControlCodeEnabled = true;
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
 
-void pre_auton(void) {
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+void pre_auton(void)
+{
+  usercontrol()
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void autonomous(void) {
-
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+void autonomous(void)
+{
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+void usercontrol(void)
+{
+  while (1)
+  {
+    controlla.Axis1.value()
 
-void usercontrol(void) {
-
-  // User control code here, inside the loop
-  while (1) {
-
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
-
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
-
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    // "wait" prevents overactivity
+    wait(20, msec);
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
-int main() {
-  // Set up callbacks for autonomous and driver control periods.
+// Startup Sequence
+int main()
+{
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
-
-  // Run the pre-autonomous function.
   pre_auton();
 
-  // Prevent main from exiting with an infinite loop.
-  while (true) {
+  while (true)
+  {
     wait(100, msec);
   }
 }
