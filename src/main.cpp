2143db35339f25106044f8660abe120d5ef85efb
23emitchell <23_emitchell@s.wmrhsd.org>
2022-10-06 15:20:08 -0400

    made the reverse button thingy

--- src/main.cpp
+++ src/main.cpp
@@ -27,79 +27,51 @@ pneumatics P1 = pneumatics(Brain.ThreeWirePort.F);
 
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+int RobotReverseVariable = 1;
+
+void RobotReverse() {
+  if (RobotReverseVariable == 1) {
+    RobotReverseVariable = -1;
+    Brain.Screen.print(RobotReverseVariable);
+    wait(250, msec);
+    Brain.Screen.clearLine();
+    return;
+  };
+  if (RobotReverseVariable == -1) {
+    RobotReverseVariable = 1;
+    Brain.Screen.print(RobotReverseVariable);
+    wait(250, msec);
+    Brain.Screen.clearLine();
+    return;
+  };
+};
 
 void pre_auton(void) {
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-}
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+}
 
 void autonomous(void) {
 
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
-
-  // User control code here, inside the loop
+  Brain.Screen.clearLine();
   while (1) {
-
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
-
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
-
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    LeftFront.spin(forward, Controller1.Axis3.position() * RobotReverseVariable, percent);
+    RightFront.spin(forward, Controller1.Axis3.position() * RobotReverseVariable, percent);
+    LeftRear.spin(forward, Controller1.Axis3.position() * RobotReverseVariable, percent);
+    RightRear.spin(forward, Controller1.Axis3.position() * RobotReverseVariable, percent);
+    if (Controller1.ButtonL1.pressing()) {
+      RobotReverse();
+    }
   }
+  wait(20, msec);
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
 int main() {
-  // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
-
-  // Run the pre-autonomous function.
   pre_auton();
-
-  // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);
   }
