31197fe26f483fe1547ec5e73989516d0c93ac96
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2022-10-09 21:44:50 -0400

    First commit for Kraken Bot
    Setting up motors and stuff.

---
+++ Kraken-Dev/include/robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor Top_Left;
+extern motor Top_Right;
+extern motor Bottom_Left;
+extern motor Bottom_Right;
+extern controller Controller1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ Kraken-Dev/src/main.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\bowma                                            */
+/*    Created:      Sun Oct 09 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Top_Left             motor         1               
+// Top_Right            motor         2               
+// Bottom_Left          motor         3               
+// Bottom_Right         motor         4               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
