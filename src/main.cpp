e4bf433aa5119bb7bae201b434bdf337a153285e
CL0520 <23_clichtenberger@s.wmrhsd.org>
2023-01-05 17:01:04 -0500

    added auton select

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,9 @@ bool RemoteControlCodeEnabled = true;
 int RobotReverseVariable = 1;
 bool manual = false;
 int amount = 0;
+int AutonSelected = 0;
+int AutonMin = 0;
+int AutonMax = 6;
 
 void StopAllChasis() {
   LeftFront.stop(hold);
@@ -51,12 +54,122 @@ void print(int per, bool man){
   Controller1.Screen.newLine();
   Controller1.Screen.print("%s", manual ? "true" : "false");
 }
+void drawGUI() {
+  // Draws 2 buttons to be used for selecting auto
+  Brain.Screen.clearScreen();
+  Brain.Screen.setFillColor(red);
+  Brain.Screen.drawRectangle(20, 50, 50, 50);
+  Brain.Screen.printAt(25, 75, "0");
+  Brain.Screen.setFillColor(orange);
+  Brain.Screen.drawRectangle(120, 50, 50, 50);
+  Brain.Screen.printAt(125, 75, "1");
+  Brain.Screen.setFillColor(yellow);
+  Brain.Screen.drawRectangle(220, 50, 50, 50);
+  Brain.Screen.printAt(225, 75, "2");
+  Brain.Screen.setFillColor(green);
+  Brain.Screen.drawRectangle(320, 50, 50, 50);
+  Brain.Screen.printAt(325, 75, "3");
+  Brain.Screen.setFillColor(blue);
+  Brain.Screen.drawRectangle(20, 150, 50, 50);
+  Brain.Screen.printAt(25, 175, "4");
+  Brain.Screen.setFillColor(purple);
+  Brain.Screen.drawRectangle(120, 150, 50, 50);
+  Brain.Screen.printAt(125, 175, "5");
+  Brain.Screen.setFillColor(ClrPink);
+  Brain.Screen.drawRectangle(220, 150, 50, 50);
+  Brain.Screen.printAt(225, 175, "6");
+  Brain.Screen.setFillColor(ClrGreenYellow);
+  Brain.Screen.drawRectangle(320, 150, 50, 50);
+  Brain.Screen.printAt(325, 175, "7");
+  Brain.Screen.setFillColor(black);
+}
 
+void selectAuton() {
+  bool selectingAuton = true;
+
+  int x = Brain.Screen.xPosition(); // get the x position of last touch of the screen
+  int y = Brain.Screen.yPosition(); // get the y position of last touch of the screen
+  // check to see if buttons were pressed
+  if (x >= 20 && x <= 70 && y >= 50 && y <= 100) // select button pressed
+  {
+    AutonSelected = 0;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+  else if (x >= 120 && x <= 170 && y >= 50 && y <= 100){
+    AutonSelected = 1;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+  else if (x >= 220 && x <= 270 && y >= 50 && y <= 100){
+    AutonSelected = 2;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+  else if (x >= 320 && x <= 370 && y >= 50 && y <= 100){
+    AutonSelected = 3;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+  else if (x >= 20 && x <= 70 && y >= 150 && y <= 200){
+    AutonSelected = 4;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+  else if (x >= 120 && x <= 170 && y >= 150 && y <= 200){
+    AutonSelected = 5;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+  else if (x >= 220 && x <= 270 && y >= 150 && y <= 200){
+    AutonSelected = 6;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+  else if (x >= 320 && x <= 370 && y >= 150 && y <= 200){
+    AutonSelected = 7;
+      
+    Brain.Screen.printAt(400, 200, "Auton: %d", AutonSelected);
+  }
+
+  wait(10, msec); // slow it down
+  Brain.Screen.setFillColor(black);
+}
 void pre_auton(void) {
   P1.open();
+  Brain.Screen.printAt(1, 40, "pre auton is running");
+  drawGUI();
+  Brain.Screen.pressed(selectAuton);
 }
 
 void autonomous(void) {
+  switch (AutonSelected) {
+  case 0:
+  //code 0
+  break;
+    case 1:
+  //code 1
+  break;
+    case 2:
+  //code 2
+  break;
+    case 3:
+  //code 3
+  break;
+    case 4:
+  //code 4
+  break;
+    case 5:
+  //code 5
+  break;
+    case 6:
+  //code 6
+  break;
+  case 7:
+  //code 7
+  break;
+  }
+
 //spin roller
 //get some disc
 //lauch them into the goal
