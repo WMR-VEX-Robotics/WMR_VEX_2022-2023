7c6c179bd6bb19874ecdda922d6b755f3dca045e
Liechtenstein42 <cl.steven520@gmail.com>
2023-03-02 20:10:47 -0500

    right auton prototype

--- src/main.cpp
+++ src/main.cpp
@@ -546,6 +546,7 @@ void autonomous(void) {
       obamatree();
       break;
     case 4:
+      // initialization of left auton
       Launch.spinTo(-90, degrees, false);
       Launch.setPosition(0, degrees);
       Vacuum.setPosition(0, degrees);
@@ -561,6 +562,8 @@ void autonomous(void) {
       RightFront.setMaxTorque(25, percent);
       LeftRear.setMaxTorque(25, percent);
       RightRear.setMaxTorque(25, percent);
+
+      // spin roller
       moveForwardWait(3, 0.5);
       Vacuum.spinToPosition(-180, degrees, false);
       wait(0.25, seconds);
@@ -570,8 +573,12 @@ void autonomous(void) {
       RightFront.setMaxTorque(100, percent);
       LeftRear.setMaxTorque(100, percent);
       RightRear.setMaxTorque(100, percent);
+
+      // launch preload
       OdomSpinTo(1.75);
+      obamatree();
       OdomSpinTo(1.75);
+      obamatree();
       wait(0.5, seconds);
       Flywheel1.spin(reverse, 10.1, volt);
       Flywheel2.spin(forward, 10.1, volt);
@@ -584,20 +591,26 @@ void autonomous(void) {
       Flywheel2.spin(forward, 10.2, volt);
       wait(0.5, seconds);
       useLauncher();
+
+      // get stack of 3
       Flywheel1.spin(reverse, 9.4, volt);
       Flywheel2.spin(forward, 9.4, volt);
-      //obamatree();
-      //OdomSpinToCoordinate(18,34);
-      //obamatree();
       Vacuum.spin(forward);
-      obamatree();
       OdomSpinTo(135);  //was 140
+      obamatree();
       OdomSpinTo(135);
+      obamatree();
       moveForward(25);
+      obamatree();
       wait(0.1, sec);
       moveForward(25);
+      obamatree();
       OdomSpinTo(31);
+      obamatree();
       OdomSpinTo(31);
+
+      // launch stack of 3
+      obamatree();
       useLauncher();
       Flywheel1.spin(reverse, 9.4, volt);
       Flywheel2.spin(forward, 9.4, volt);
@@ -609,9 +622,59 @@ void autonomous(void) {
       useLauncher();
       Flywheel1.spin(reverse, 0, volt);
       Flywheel2.spin(forward, 0, volt);
+
+      // prep for driver
+      OdomSpinTo(149);
+      obamatree();
+      OdomSpinTo(149);
+      obamatree();
       break;
     case 5:
-      // spin left roller, shoot, get more discs, shoot
+      // initialization of right auton
+      Launch.spinTo(-90, degrees, false);
+      Launch.setPosition(0, degrees);
+      Vacuum.setPosition(0, degrees);
+      obamatree();
+      LeftFront.setVelocity(100, percent);
+      RightFront.setVelocity(100, percent);
+      LeftRear.setVelocity(100, percent);
+      RightRear.setVelocity(100, percent);
+      Vacuum.setVelocity(100, percent);
+      Flywheel1.spin(reverse, 15, volt);
+      Flywheel2.spin(forward, 15, volt);
+      LeftFront.setMaxTorque(25, percent);
+      RightFront.setMaxTorque(25, percent);
+      LeftRear.setMaxTorque(25, percent);
+      RightRear.setMaxTorque(25, percent);
+
+      // get roller
+      moveBackwards(12);
+      obamatree();
+      OdomSpinTo(270);
+      moveBackwards(4);
+      obamatree();
+      Vacuum.spinFor(-180, degrees, false);
+      wait(0.5, sec);
+
+      // launch preload
+      Flywheel1.spin(reverse, 10.1, volt);
+      Flywheel2.spin(forward, 10.1, volt);
+      wait(0.5, seconds);
+      useLauncher();
+      Flywheel1.spin(reverse, 10.2, volt);
+      Flywheel2.spin(forward, 10.2, volt);
+      wait(0.5, seconds);
+      Flywheel1.spin(reverse, 10.2, volt);
+      Flywheel2.spin(forward, 10.2, volt);
+      wait(0.5, seconds);
+      useLauncher();
+
+      //prep for driver
+      wait(0.5, sec);
+      moveForwardWait(2, 0.5);
+      obamatree();
+      OdomSpinTo(90);
+      obamatree();
       break;
     case 6:
       // spin right roller, shoot, get more discs, shoot
